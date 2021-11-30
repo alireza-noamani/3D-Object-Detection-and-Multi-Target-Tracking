ddab5edfdcf37210d4357079f96b3151c51c9c7e
Alireza Noamani <noamani@ualberta.ca>
2021-11-30 11:41:10 -0500

    Complete steps 1-2

--- loop_over_dataset.py
+++ loop_over_dataset.py
@@ -53,16 +53,16 @@ import misc.params as params
 data_filename = 'training_segment-1005081002024129653_5313_150_5333_150_with_camera_labels.tfrecord' # Sequence 1
 # data_filename = 'training_segment-10072231702153043603_5725_000_5745_000_with_camera_labels.tfrecord' # Sequence 2
 # data_filename = 'training_segment-10963653239323173269_1924_000_1944_000_with_camera_labels.tfrecord' # Sequence 3
-show_only_frames = [0, 200] # show only frames in interval for debugging
+show_only_frames = [0, 1] # show only frames in interval for debugging
 
 ## Prepare Waymo Open Dataset file for loading
 data_fullpath = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'dataset', data_filename) # adjustable path in case this script is called from another working directory
-results_fullpath = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'results')
+results_fullpath = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'results/darknet model/results_sequence_1_darknet/')
 datafile = WaymoDataFileReader(data_fullpath)
 datafile_iter = iter(datafile)  # initialize dataset iterator
 
 ## Initialize object detection
-configs_det = det.load_configs(model_name='fpn_resnet') # options are 'darknet', 'fpn_resnet'
+configs_det = det.load_configs(model_name='darknet') # options are 'darknet', 'fpn_resnet'
 model_det = det.create_model(configs_det)
 
 configs_det.use_labels_as_objects = False # True = use groundtruth labels as objects, False = use model-based detection
@@ -79,7 +79,7 @@ camera = None # init camera sensor object
 np.random.seed(10) # make random values predictable
 
 ## Selective execution and visualization
-exec_detection = ['bev_from_pcl', 'detect_objects', 'validate_object_labels', 'measure_detection_performance'] # options are 'bev_from_pcl', 'detect_objects', 'validate_object_labels', 'measure_detection_performance'; options not in the list will be loaded from file
+exec_detection = ['bev_from_pcl'] # options are 'bev_from_pcl', 'detect_objects', 'validate_object_labels', 'measure_detection_performance'; options not in the list will be loaded from file
 exec_tracking = [] # options are 'perform_tracking'
 exec_visualization = [] # options are 'show_range_image', 'show_bev', 'show_pcl', 'show_labels_in_image', 'show_objects_and_labels_in_bev', 'show_objects_in_bev_labels_in_camera', 'show_tracks', 'show_detection_performance', 'make_tracking_movie'
 exec_list = make_exec_list(exec_detection, exec_tracking, exec_visualization)
@@ -129,7 +129,7 @@ while True:
             print('loading lidar point-cloud from result file')
             lidar_pcl = load_object_from_file(results_fullpath, data_filename, 'lidar_pcl', cnt_frame)
             
-        ## Compute lidar birds-eye view (bev)
+        # ## Compute lidar birds-eye view (bev)
         if 'bev_from_pcl' in exec_list:
             print('computing birds-eye view from lidar pointcloud')
             lidar_bev = pcl.bev_from_pcl(lidar_pcl, configs_det)
@@ -137,43 +137,43 @@ while True:
             print('loading birds-eve view from result file')
             lidar_bev = load_object_from_file(results_fullpath, data_filename, 'lidar_bev', cnt_frame)
 
-        ## 3D object detection
-        if (configs_det.use_labels_as_objects==True):
-            print('using groundtruth labels as objects')
-            detections = tools.convert_labels_into_objects(frame.laser_labels, configs_det)
-        else:
-            if 'detect_objects' in exec_list:
-                print('detecting objects in lidar pointcloud')   
-                detections = det.detect_objects(lidar_bev, model_det, configs_det)
-            else:
-                print('loading detected objects from result file')
-                # load different data for final project vs. mid-term project
-                if 'perform_tracking' in exec_list:
-                    detections = load_object_from_file(results_fullpath, data_filename, 'detections', cnt_frame)
-                else:
-                    detections = load_object_from_file(results_fullpath, data_filename, 'detections_' + configs_det.arch + '_' + str(configs_det.conf_thresh), cnt_frame)
+        # ## 3D object detection
+        # if (configs_det.use_labels_as_objects==True):
+        #     print('using groundtruth labels as objects')
+        #     detections = tools.convert_labels_into_objects(frame.laser_labels, configs_det)
+        # else:
+        #     if 'detect_objects' in exec_list:
+        #         print('detecting objects in lidar pointcloud')   
+        #         detections = det.detect_objects(lidar_bev, model_det, configs_det)
+        #     else:
+        #         print('loading detected objects from result file')
+        #         # load different data for final project vs. mid-term project
+        #         if 'perform_tracking' in exec_list:
+        #             detections = load_object_from_file(results_fullpath, data_filename, 'detections', cnt_frame)
+        #         else:
+        #             detections = load_object_from_file(results_fullpath, data_filename, 'detections_' + configs_det.arch + '_' + str(configs_det.conf_thresh), cnt_frame)
 
         ## Validate object labels
-        if 'validate_object_labels' in exec_list:
-            print("validating object labels")
-            valid_label_flags = tools.validate_object_labels(frame.laser_labels, lidar_pcl, configs_det, 0 if configs_det.use_labels_as_objects==True else 10)
-        else:
-            print('loading object labels and validation from result file')
-            valid_label_flags = load_object_from_file(results_fullpath, data_filename, 'valid_labels', cnt_frame)            
+        # if 'validate_object_labels' in exec_list:
+        #     print("validating object labels")
+        #     valid_label_flags = tools.validate_object_labels(frame.laser_labels, lidar_pcl, configs_det, 0 if configs_det.use_labels_as_objects==True else 10)
+        # else:
+        #     print('loading object labels and validation from result file')
+        #     valid_label_flags = load_object_from_file(results_fullpath, data_filename, 'valid_labels', cnt_frame)            
 
-        ## Performance evaluation for object detection
-        if 'measure_detection_performance' in exec_list:
-            print('measuring detection performance')
-            det_performance = eval.measure_detection_performance(detections, frame.laser_labels, valid_label_flags, configs_det.min_iou)     
-        else:
-            print('loading detection performance measures from file')
-            # load different data for final project vs. mid-term project
-            if 'perform_tracking' in exec_list:
-                det_performance = load_object_from_file(results_fullpath, data_filename, 'det_performance', cnt_frame)
-            else:
-                det_performance = load_object_from_file(results_fullpath, data_filename, 'det_performance_' + configs_det.arch + '_' + str(configs_det.conf_thresh), cnt_frame)   
+        # ## Performance evaluation for object detection
+        # if 'measure_detection_performance' in exec_list:
+        #     print('measuring detection performance')
+        #     det_performance = eval.measure_detection_performance(detections, frame.laser_labels, valid_label_flags, configs_det.min_iou)     
+        # else:
+        #     print('loading detection performance measures from file')
+        #     # load different data for final project vs. mid-term project
+        #     if 'perform_tracking' in exec_list:
+        #         det_performance = load_object_from_file(results_fullpath, data_filename, 'det_performance', cnt_frame)
+        #     else:
+        #         det_performance = load_object_from_file(results_fullpath, data_filename, 'det_performance_' + configs_det.arch + '_' + str(configs_det.conf_thresh), cnt_frame)   
 
-        det_performance_all.append(det_performance) # store all evaluation results in a list for performance assessment at the end
+        # det_performance_all.append(det_performance) # store all evaluation results in a list for performance assessment at the end
         
 
         ## Visualization for object detection
