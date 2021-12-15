00080e340a0dccf8290e1fdb211cf4b7ec941825
Alireza Noamani <noamani@ualberta.ca>
2021-12-15 15:38:58 -0500

    Complete MTT step 4

--- student/measurements.py
+++ student/measurements.py
@@ -47,8 +47,14 @@ class Sensor:
         # TODO Step 4: implement a function that returns True if x lies in the sensor's field of view, 
         # otherwise False.
         ############
-
-        return True
+        pos_veh = np.ones((4, 1))
+        pos_veh[0:3] = x[0:3]
+        pos_sens = self.veh_to_sens * pos_veh
+        alpha = np.arctan(pos_sens[1]/pos_sens[0])
+        if alpha > self.fov[0] and alpha < self.fov[1]:
+            return True
+        else:
+            return False
         
         ############
         # END student code
@@ -70,8 +76,17 @@ class Sensor:
             # - make sure to not divide by zero, raise an error if needed
             # - return h(x)
             ############
+            pos_veh = np.ones((4,1))
+            pos_veh[0:3] = x[0:3]
+            pos_sens = self.veh_to_sens * pos_veh
+            if pos_sens[0] == 0:
+                raise NameError('Jacobian not defined for x[0]=0!')
+            else:
+                hx = np.zeros((2, 1))
+                hx[0, 0] = self.c_i - self.f_i * pos_sens[1, 0] / pos_sens[0, 0]
+                hx[1, 0] = self.c_j - self.f_j * pos_sens[2, 0] / pos_sens[0, 0]
 
-            pass
+            return hx
         
             ############
             # END student code
@@ -115,9 +130,9 @@ class Sensor:
         # TODO Step 4: remove restriction to lidar in order to include camera as well
         ############
         
-        if self.name == 'lidar':
-            meas = Measurement(num_frame, z, self)
-            meas_list.append(meas)
+        # if self.name == 'lidar':
+        meas = Measurement(num_frame, z, self)
+        meas_list.append(meas)
         return meas_list
         
         ############
@@ -155,9 +170,15 @@ class Measurement:
             ############
             # TODO Step 4: initialize camera measurement including z and R 
             ############
+            sigma_cam_i = params.sigma_cam_i
+            sigma_cam_j = params.sigma_cam_j
 
-            pass
-        
+            self.R = np.matrix([[sigma_cam_i**2, 0],
+                                [0, sigma_cam_j**2]])
+            self.z = np.zeros((sensor.dim_meas, 1))
+            self.z[0] = z[0]
+            self.z[1] = z[1]
+
             ############
             # END student code
             ############ 
